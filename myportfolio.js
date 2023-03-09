f13465ae3e937153a2c03e34fec631caf980c633
ariskafebrilianti <bncliany@gmail.com>
2023-03-09 21:35:05 +0700

    update12

--- myportfolio.js
+++ myportfolio.js
@@ -10,3 +10,32 @@ const closeIcon = document.getElementById("closeModalIcon");
     closeIcon.addEventListener('click', () => {
     popup.style.display = "none";
     });
+
+    const popup2 = document.getElementById("myPopup2")
+    const openImage2 = document.getElementById("openModalButton2")
+    const closeIcon2 = document.getElementById("closeModalIcon2");
+    
+        //Ketika tombol openButton ditekan
+        openImage2.addEventListener('click', () => {
+        popup2.style.display = "block";
+        });
+        // Ketika tombol closeButton ditekan
+        closeIcon2.addEventListener('click', () => {
+        popup2.style.display = "none";
+        });
+
+        const popup3 = document.getElementById("myPopup3")
+        const openImage3 = document.getElementById("openModalButton3")
+        const closeIcon3 = document.getElementById("closeModalIcon3");
+        
+            //Ketika tombol openButton ditekan
+            openImage3.addEventListener('click', () => {
+            popup3.style.display = "block";
+            });
+            // Ketika tombol closeButton ditekan
+            closeIcon3.addEventListener('click', () => {
+            popup3.style.display = "none";
+            });
+    
+
+       
